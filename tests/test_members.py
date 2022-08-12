a06fd5a8122aba52d5233e00b1330803fd18c254
Riad <riad.ladjel@quadratic-labs.com>
2022-08-12 11:35:34 +0200

    FIX : fixing conftest such that the generic contract takes into account members and proposals addition

--- tests/test_members.py
+++ tests/test_members.py
@@ -13,8 +13,8 @@ async def test_is_member(contract):
     assert return_value.result.success == 0
 
     # adding two fake members
-    await contract.add_member((1, 1, 1, 1, 1, 1)).invoke()
-    await contract.add_member((2, 2, 2, 2, 2, 2)).invoke()
+    await contract.add_member((6, 6, 1, 1, 1, 1)).invoke()
+    await contract.add_member((7, 7, 2, 2, 2, 2)).invoke()
 
     # is_member on any of the 2 members should return True
     return_value = await contract.is_member(1).invoke()
@@ -24,5 +24,5 @@ async def test_is_member(contract):
     assert return_value.result.success == 1
 
     # is_member on a non-existing member should return False
-    return_value = await contract.is_member(3).invoke()
+    return_value = await contract.is_member(8).invoke()
     assert return_value.result.success == 0
