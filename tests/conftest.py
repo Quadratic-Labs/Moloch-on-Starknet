62eead12acc267cedef5fea998bd6d37ef289de2
Riad <riad.ladjel@quadratic-labs.com>
2022-08-29 17:03:50 +0200

    FIX: fixing add_role parameters order in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -86,8 +86,6 @@ async def empty_contract(starknet, test_contracts):
     )
 
 
-
-
 @dataclass
 class Member:
     address: int
@@ -159,69 +157,69 @@ MEMBERS: list[Member] = [
 ]
 
 
-
 PROPOSALS: list[Proposal] = [
-    Proposal( # Submitted and vote + grace open
+    Proposal(  # Submitted and vote + grace open
         id=1,
         type=1,
         submittedBy=1,
         submittedAt=1,
-        votingEndsAt=1664575200, #Oct 01 2022 00:00:00
-        graceEndsAt=1661983200, #Sep 01 2022 00:00:00
+        votingEndsAt=1664575200,  # Oct 01 2022 00:00:00
+        graceEndsAt=1661983200,  # Sep 01 2022 00:00:00
         expiresAt=1,
         quorum=3,
         majority=3,
         yesVotes=0,
         noVotes=0,
-        status=1, # SUBMITTED
+        status=1,  # SUBMITTED
         description=1,
     ),
-    Proposal(# ACCEPTED and vote closed
+    Proposal(  # ACCEPTED and vote closed
         id=2,
         type=1,
         submittedBy=3,
         submittedAt=1,
-        votingEndsAt=1659304800, #Aug 01 2022 00:00:00
-        graceEndsAt=1656626400,  #Jul 01 2022 00:00:00
+        votingEndsAt=1659304800,  # Aug 01 2022 00:00:00
+        graceEndsAt=1656626400,  # Jul 01 2022 00:00:00
         expiresAt=1,
         quorum=2,
         majority=3,
         yesVotes=3,
         noVotes=2,
-        status=2, #ACCEPTED
+        status=2,  # ACCEPTED
         description=1,
-    ),    
-    Proposal(# Rejected and vote closed
+    ),
+    Proposal(  # Rejected and vote closed
         id=3,
         type=1,
         submittedBy=1,
         submittedAt=1,
-        votingEndsAt=1659304800, #Aug 01 2022 00:00:00
-        graceEndsAt=1656626400,  #Jul 01 2022 00:00:00
+        votingEndsAt=1659304800,  # Aug 01 2022 00:00:00
+        graceEndsAt=1656626400,  # Jul 01 2022 00:00:00
         expiresAt=1,
         quorum=3,
         majority=3,
         yesVotes=2,
         noVotes=3,
-        status=3, #REJECTED
+        status=3,  # REJECTED
         description=1,
     ),
-    Proposal( # Submitted and vote open + grace closed
+    Proposal(  # Submitted and vote open + grace closed
         id=4,
         type=1,
         submittedBy=3,
         submittedAt=1,
-        votingEndsAt=1664575200, #Oct 01 2022 00:00:00
-        graceEndsAt=1659304800, #Aug 01 2022 00:00:00
+        votingEndsAt=1664575200,  # Oct 01 2022 00:00:00
+        graceEndsAt=1659304800,  # Aug 01 2022 00:00:00
         expiresAt=1,
         quorum=3,
         majority=3,
         yesVotes=0,
         noVotes=0,
-        status=1, # SUBMITTED
+        status=1,  # SUBMITTED
         description=1,
-    ),    
+    ),
 ]
+
 
 @pytest.fixture
 async def contract(starknet, test_contracts):
@@ -246,13 +244,13 @@ async def contract(starknet, test_contracts):
         3: [govern],
         4: [govern],
         5: [],
-}
+    }
     for member in MEMBERS:
         await contract.add_member(astuple(member)).invoke()
         for role in MEMBER_ROLES[member.address]:
-            await contract.add_role(role, member.address).invoke()
+            await contract.add_role(member.address, role).invoke()
 
     for proposal in PROPOSALS:
         await contract.add_proposal(astuple(proposal)).invoke()
-        
+
     return contract
